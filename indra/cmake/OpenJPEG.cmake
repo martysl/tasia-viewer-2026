f95c74666b40c46d4d064e394802df33fd29dbcf
tasia <tasia@easierit.org>
2026-09-02 18:01:38 +0200

    fix(arm64): OpenJPEG via find_library/find_path (no imported target)

--- indra/cmake/OpenJPEG.cmake
+++ indra/cmake/OpenJPEG.cmake
@@ -8,8 +8,10 @@ add_library( ll::openjpeg INTERFACE IMPORTED )
 
 use_system_binary(openjpeg)
 if (LL_LINUX_ARM64)
-  find_package(OpenJPEG REQUIRED)
-  target_link_libraries(ll::openjpeg INTERFACE OpenJPEG::OpenJPEG)
+  find_library(OPENJPEG_LIBRARY NAMES openjp2 REQUIRED)
+  find_path(OPENJPEG_INCLUDE_DIR openjpeg.h PATH_SUFFIXES openjpeg-2.5 openjpeg-2.4 openjpeg-2.3 openjpeg REQUIRED)
+  target_link_libraries(ll::openjpeg INTERFACE ${OPENJPEG_LIBRARY})
+  target_include_directories(ll::openjpeg SYSTEM INTERFACE ${OPENJPEG_INCLUDE_DIR})
   return()
 endif ()
 
