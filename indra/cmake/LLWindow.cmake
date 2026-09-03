3ae572af1273f9db819a45cd76252bb8f7e32f75
tasia <tasia@easierit.org>
2026-09-03 19:58:00 +0200

    build(arm64): use native SDL2 on Linux

--- indra/cmake/LLWindow.cmake
+++ indra/cmake/LLWindow.cmake
@@ -29,11 +29,23 @@ if (LINUX)
   else()
     target_compile_definitions( ll::SDL INTERFACE LL_SDL2=1 LL_SDL=1 )
 
-    use_system_binary(SDL2)
-    use_prebuilt_binary(SDL2)
+    if (ARCH STREQUAL "aarch64")
+      find_path(SDL2_INCLUDE_ROOT
+        NAMES SDL2/SDL.h
+        PATHS /usr/include
+        REQUIRED NO_DEFAULT_PATH)
+      find_library(SDL2_LIBRARY
+        NAMES SDL2
+        PATHS /usr/lib/${DPKG_ARCH}
+        REQUIRED NO_DEFAULT_PATH)
+      target_include_directories(ll::SDL SYSTEM INTERFACE ${SDL2_INCLUDE_ROOT})
+      target_link_libraries(ll::SDL INTERFACE ${SDL2_LIBRARY} X11)
+    else()
+      use_system_binary(SDL2)
+      use_prebuilt_binary(SDL2)
+      target_link_libraries(ll::SDL INTERFACE SDL2 X11)
+    endif()
     set (SDL2_FOUND TRUE)
-
-    target_link_libraries( ll::SDL INTERFACE SDL2 X11 )
   endif()
 endif (LINUX)
 
